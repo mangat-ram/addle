e6eea4bf5b949a195f93fc61ecb4f99d09a7ffea
mangat-ram <se.mangat.ram@gmail.com>
2024-04-01 21:28:06 +0530

    Setting up Database.

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -2,3 +2,18 @@ import {drizzle} from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as dotenv from 'dotenv';
 import * as schema from '../../../migrations/schema';
+dotenv.config({path: '.env'});
+
+if(!process.env.DATABASE_URL){
+  console.log('No database URL.');
+}
+
+const client = postgres(process.env.DATABASE_URL as string, {max:1})
+const db = drizzle(client, { schema });
+const migrateDb = async () => {
+  console.log("Migrating Client.");
+  await migrateDb
+  
+}
+
+export default db;
