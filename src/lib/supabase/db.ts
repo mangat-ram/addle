c831ba2d4d4e0fe9e354f45063ce1fcb05ec1eea
mangat-ram <se.mangat.ram@gmail.com>
2024-04-01 21:35:21 +0530

    Some Updations.

--- src/lib/supabase/db.ts
+++ src/lib/supabase/db.ts
@@ -2,6 +2,7 @@ import {drizzle} from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as dotenv from 'dotenv';
 import * as schema from '../../../migrations/schema';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
 dotenv.config({path: '.env'});
 
 if(!process.env.DATABASE_URL){
@@ -11,9 +12,14 @@ if(!process.env.DATABASE_URL){
 const client = postgres(process.env.DATABASE_URL as string, {max:1})
 const db = drizzle(client, { schema });
 const migrateDb = async () => {
-  console.log("Migrating Client.");
-  await migrateDb
-  
+  try {
+    console.log("Migrating Client.");
+    await migrate(db, { migrationsFolder : "migrations" })
+    console.log("Successfully Migrated.");
+  } catch (error) {
+    console.log("Error Migrating Client."); 
+  }
 }
+migrateDb();
 
 export default db;
