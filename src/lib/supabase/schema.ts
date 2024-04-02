b96181eb87a57f52e21bdd0acb6ecef7ea5d5277
mangat-ram <se.mangat.ram@gmail.com>
2024-04-02 22:34:19 +0530

    Completed Schema Setup.

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -1,4 +1,8 @@
-import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { jsonb, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { prices, subscriptionStatus, users } from "../../../migrations/schema";
+import { integer } from "drizzle-orm/pg-core";
+import { boolean } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
 export const workspaces = pgTable('workspaces', {
   id: uuid('id').defaultRandom().primaryKey().notNull(),
@@ -49,3 +53,21 @@ export const files = pgTable('files', {
     onDelete:'cascade'
   })
 })
+
+export const subscriptions = pgTable("subscriptions", {
+	id: text("id").primaryKey().notNull(),
+	userId: uuid("user_id").notNull().references(() => users.id),
+	status: subscriptionStatus("status"),
+	metadata: jsonb("metadata"),
+	priceId: text("price_id").references(() => prices.id),
+	quantity: integer("quantity"),
+	cancelAtPeriodEnd: boolean("cancel_at_period_end"),
+	created: timestamp("created", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+	currentPeriodStart: timestamp("current_period_start", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+	currentPeriodEnd: timestamp("current_period_end", { withTimezone: true, mode: 'string' }).default(sql`now()`).notNull(),
+	endedAt: timestamp("ended_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+	cancelAt: timestamp("cancel_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+	canceledAt: timestamp("canceled_at", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+	trialStart: timestamp("trial_start", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+	trialEnd: timestamp("trial_end", { withTimezone: true, mode: 'string' }).default(sql`now()`),
+});
