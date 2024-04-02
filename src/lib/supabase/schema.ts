e6572f8bc6a1c953e63a88f24e9b4be4725ec832
mangat-ram <se.mangat.ram@gmail.com>
2024-04-02 22:14:50 +0530

    Integrated Schema of Stripe Integration.

--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -14,3 +14,38 @@ export const workspaces = pgTable('workspaces', {
   logo: text('logo'),
   bannerUrl: text('banner_url'),
 });
+
+export const folders = pgTable('folders', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('createdAt', {
+    withTimezone:true,
+    mode: 'string',
+  }),
+  title: text('title').notNull(),
+  iconId: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  bannerUrl: text('banner_url'),
+  workspaceId: uuid('workspace_id').references(() => workspaces.id,{
+    onDelete:'cascade'
+  })
+})
+
+export const files = pgTable('files', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  createdAt: timestamp('createdAt', {
+    withTimezone:true,
+    mode: 'string',
+  }),
+  title: text('title').notNull(),
+  iconId: text('icon_id').notNull(),
+  data: text('data'),
+  inTrash: text('in_trash'),
+  bannerUrl: text('banner_url'),
+  workspaceId: uuid('workspace_id').references(() => workspaces.id,{
+    onDelete:'cascade'
+  }),
+  folderId: uuid('folder_id').references(() => folders.id, {
+    onDelete:'cascade'
+  })
+})
