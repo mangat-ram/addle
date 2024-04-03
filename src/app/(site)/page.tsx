e70840e620d52186db397708d686fd3760334269
mangat-ram <se.mangat.ram@gmail.com>
2024-04-03 23:01:55 +0530

    Added the Clients Data.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Banner from '../../../public/appBanner.png';
+import { CLIENTS } from '@/lib/constants';
 
 function HomePage() {
   return (
@@ -30,7 +31,19 @@ function HomePage() {
         </div>
       </section>
       <section className='relative'>
-        <div className='overflow-hidden flex absolute after:content-[" "] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:top-0 after:w-20  after:z-10 after:absolute before:content-[" "] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:top-0 before:bottom-0 before:w-20 before:z-10 before:absolute'></div>
+        <div className='overflow-hidden flex absolute after:content-[" "] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:top-0 after:w-20  after:z-10 after:absolute before:content-[" "] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:top-0 before:bottom-0 before:w-20 before:z-10 before:absolute'>
+          {[...Array(2)].map((arr) => <div 
+            key={arr}
+            className='flex flex-nowrap animate-slide'
+            >
+              {CLIENTS.map((client) => (<div 
+                  key={client.alt}
+                  className='relative w-[200px] m-20 shrink-0 flex items-center'
+                  >
+                    <Image src={client.logo} alt={client.alt} width={200} className='object-contain max-w-none'/>
+                </div>))}
+          </div>)}
+        </div>
       </section>
     </>
   )
