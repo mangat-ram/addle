58a53fbb2558033b26d6b36c21fa24173b809856
mangat-ram <se.mangat.ram@gmail.com>
2024-04-03 22:14:31 +0530

    Added Banner.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,8 @@
-import TitleSection from '@/components/landing-page/title-section'
-import React from 'react'
+import TitleSection from '@/components/landing-page/title-section';
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Banner from '../../../public/appBanner.png';
 
 function HomePage() {
   return (
@@ -12,14 +15,18 @@ function HomePage() {
           title='All-In-One Collaboration and Productivity Platform' 
         />
         <div
-          className='bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]'
+          className='bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]'
         >
-          <button 
-            variant='btn-secondary'
+          <Button 
+            variant='secondary'
             className='w-full rounded-[10px] p-6 text-2xl bg-background hover:bg-amber-200'
           >
             Get Addle Free
-          </button>
+          </Button>
+        </div>
+        <div className='md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]'>
+          <Image src={Banner} alt="appBanner" />
+          <div className='bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10'></div>
         </div>
       </div>
     </section>
