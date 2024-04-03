7b874aaeaee94c5254e3a973e4a54a20ab28c7f4
mangat-ram <se.mangat.ram@gmail.com>
2024-04-03 22:00:24 +0530

    added Button styles.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -8,9 +8,19 @@ function HomePage() {
         className='overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center'
       >
         <TitleSection 
-          pill='⭐ Your Workspace Perfected' 
+          pill='⭐ Your Workspace Perfected ' 
           title='All-In-One Collaboration and Productivity Platform' 
         />
+        <div
+          className='bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]'
+        >
+          <button 
+            variant='btn-secondary'
+            className='w-full rounded-[10px] p-6 text-2xl bg-background hover:bg-amber-200'
+          >
+            Get Addle Free
+          </button>
+        </div>
       </div>
     </section>
   )
