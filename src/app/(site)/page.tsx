b63349c2e455c78a1ee369c948d40ba1ca1cd48c
mangat-ram <se.mangat.ram@gmail.com>
2024-04-04 19:07:20 +0530

    Completed Pricing Section.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,15 +4,18 @@ import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Banner from '../../../public/appBanner.png';
 import Cal from '../../../public/cal.png';
-import { CLIENTS } from '@/lib/constants';
+import Diamond from '../../../public/icons/diamond.svg';
+import Check from '../../../public/icons/check.svg';
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS } from '@/lib/constants';
 import { randomUUID } from 'crypto';
 import { USERS } from '@/lib/constants';
 import { twMerge } from 'tailwind-merge';
 import { clsx } from 'clsx';
 import CustomCard from '@/components/landing-page/custom-card';
 import { Avatar,AvatarImage,AvatarFallback } from '@/components/ui/avatar';
-import { CardTitle } from '@/components/ui/card';
+import { CardContent, CardTitle } from '@/components/ui/card';
 import { CardDescription } from '@/components/ui/card';
+
 
 function HomePage() {
   return (
@@ -135,6 +138,66 @@ function HomePage() {
                 ></CustomCard>
               ))}
             </div>
+          ))}
+        </div>
+      </section>
+      <section
+        className='mt-20 px-4 sm:px-6'
+      >
+        <TitleSection
+          title="The Perfect Plan For You"
+          subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+          pill="Pricing"
+        />
+        <div
+          className='flex flex-col-reverse sm:flex-row gap-4 justify-center sm:items-stretch items-center mt-10'
+        >
+          {PRICING_CARDS.map((card) => (
+            <CustomCard 
+              key={card.planType}
+              className={clsx('w-[300px] rounded-2xl bg-amber-100 dark:bg-black/95 background-blur-3xl relative',{'border-[3px] border-brand-primaryPurple/70':card.planType === PRICING_PLANS.proplan})}
+              cardHeader={
+                <CardTitle className='text-3xl font-semibold'>{card.planType ===    PRICING_PLANS.proplan && (<>
+                  <div
+                    className='hidden dark:block w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple -z-10 top-0'
+                  />
+                  <Image src={Diamond} alt='Pro plan icon' className='absolute top-6 right-6' />
+                  </>)}
+                    {card.planType}
+                  </CardTitle>
+              }
+              cardContent = {
+                <CardContent
+                  className='p-0'
+                >
+                  <span className='font-normal text-2xl'>${card.price}</span>
+                  {+card.price > 0 ? (<span className='dark:text-washed-purple-800 ml-1'>/mo</span>): ('')}
+                  <p className='dark:text-washed-purple-800'></p>
+                  <Button 
+                    variant={'secondary'}
+                    className='whitespace-nowrap w-full mt-4 border-[3px] border-amber-300'>
+                    {card.planType === PRICING_PLANS.proplan ? 'Go Pro' : 'Get Started'}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul className='font-normal flex mb-2 flex-col gap-4'>
+                  <small>
+                    {card.highlightFeature}
+                  </small>
+                  {card.freatures.map(feature => (
+                    <li
+                      key={feature}
+                      className='flex items-center gap-2'
+                    >
+                      <Image src={Check} alt='checkIcon' />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }  
+            >
+            </CustomCard>
           ))}
         </div>
       </section>
