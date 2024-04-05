f8605f54ae670acd8cd97bb795fbe4fe6458215e
mangat-ram <se.mangat.ram@gmail.com>
2024-04-05 19:41:06 +0530

    created middleware : status= in progress.

---
+++ src/app/(main)/dashboard/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface LayoutProps {
+  children: React.ReactNode;
+  params: any; 
+}
+
+const Layout:React.FC<LayoutProps> = (
+  {
+    children,
+    params
+  }
+) => {
+  return (
+    <main
+      className='flex overflow-hidden h-screen'
+    >
+      {children}
+    </main>
+  )
+}
+
+export default Layout;
