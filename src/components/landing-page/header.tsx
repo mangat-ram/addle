1a0ff07da46dde1123b2b8e3c0bf2bf9fa0692dc
mangat-ram <se.mangat.ram@gmail.com>
2024-04-05 12:27:55 +0530

    Progress Commit : Header

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -4,6 +4,18 @@ import React, { useState } from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../../../public/cypresslogo.svg'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { cn } from '@/lib/utils';
+
 
 const routes = [
   {
@@ -87,6 +99,41 @@ const Header = () => {
           addle.co
         </span>
       </Link>
+      <NavigationMenu
+        className='hidden md:block'
+      >
+        <NavigationMenuList
+          className='gap-6'
+        >
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              onClick={() => setPath('#resources')}
+              className={cn({
+                'dark:text-white' : path === '#resources',
+                'dark:text-white/40': path !== '#resources',
+                'font-normal': true,
+                'text-xl':true 
+              })}
+            >
+              Resources
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul
+                className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'
+              >
+                <li
+                  className='row-span-3'
+                >
+                  <span 
+                    className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'>
+                      Welcome
+                    </span>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </header>
   )
 }
