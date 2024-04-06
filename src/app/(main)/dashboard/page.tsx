910eaf003161d3f632ae87637f187698f69a4d80
mangat-ram <se.mangat.ram@gmail.com>
2024-04-06 13:10:15 +0530

    Added types for the supabase.

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import db from '@/lib/supabase/db';
+import { redirect } from 'next/navigation';
+import DashboardSetup from '@/components/dashboard-setup/dashboard-setup';
 
-const Page = () => {
+const DashboardPage = async () => {
+
+  const supabase = createServerComponentClient({cookies});
+
+  const {
+    data:{ user },
+  } = await supabase.auth.getUser();
+
+  if(!user) return;
+
+  const workspace = await db.query.workspaces.findFirst(
+    {
+      where:(workspace,{ eq }) => eq(workspace.workspaceOwner, user.id),
+    }
+  )
+
+  if(!workspace){
+    return (
+      <div
+        className='bg-background h-screen w-screen flex justify-center items-center'
+      >
+        <DashboardSetup></DashboardSetup>
+      </div>
+    )
+  }
+
+  redirect(`/dashboard/${workspace.id}`)
+
   return (
-    <div>Page</div>
+    <div>DashboardPage</div>
   )
 }
 
-export default Page;
+export default DashboardPage;
---
+++ src/components/dashboard-setup/dashboard-setup.tsx
@@ -0,0 +1,45 @@
+import { AuthUser } from '@supabase/supabase-js'
+import React from 'react'
+import { 
+  Card, 
+  CardContent, 
+  CardDescription, 
+  CardHeader, 
+  CardTitle 
+} from '../ui/card';
+
+interface DashboardSetupProps{
+  user: AuthUser;
+  subscription: {} | null;
+}
+
+const DashboardSetup:React.FC<DashboardSetupProps> = ({
+  user,
+  subscription
+}) => { 
+  return (
+    <Card
+      className='w-[800px] h-screen sm:h-auto'
+    >
+      <CardHeader>
+        <CardTitle>
+          Create Workspace
+        </CardTitle>
+        <CardDescription>
+          Lets create a private workspace to get you started. you can add collaborators later from the workspace settings Tab.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={() => {}}>
+          <div
+            className='flex flex-col gap-4'
+          >
+
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default DashboardSetup;
