07c9f078c18e2615ccab838919964b4bd1760fb6
mangat-ram <se.mangat.ram@gmail.com>
2024-04-07 18:16:40 +0530

    Added workspace Schema.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,3 +6,13 @@ export const FormSchema = z.object(
       password: z.string().describe('Password').min(1, 'Password is Required'),
     }
   )
+
+export const CreateWorkspaceSchema = z.object(
+  {
+    workspaceName: z
+      .string()
+      .describe('Workspace Name')
+      .min(1,'workspace name must be of one letter'),
+    file: z.any(),
+  }
+);
