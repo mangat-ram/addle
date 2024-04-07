010d1f8a66bb9ca8385eb73968a47723e1f0168e
mangat-ram <se.mangat.ram@gmail.com>
2024-04-07 13:14:27 +0530

    add workspace form .

--- src/components/dashboard-setup/dashboard-setup.tsx
+++ src/components/dashboard-setup/dashboard-setup.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { AuthUser } from '@supabase/supabase-js'
-import React from 'react'
+import React, { useState } from 'react'
 import { 
   Card, 
   CardContent, 
@@ -9,16 +9,31 @@ import {
   CardHeader, 
   CardTitle 
 } from '../ui/card';
+import EmojiPicker from '../global/emoji-picker';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+import { FieldValues, useForm } from 'react-hook-form';
+import { Subscription } from '@/lib/supabase/supabase.types';
 
 interface DashboardSetupProps{
   user: AuthUser;
-  subscription: {} | null;
+  subscription: Subscription | null;
 }
 
 const DashboardSetup:React.FC<DashboardSetupProps> = ({
   user,
   subscription
 }) => { 
+
+  const [selectedEmoji, setSelectedEmoji] = useState('')
+  const {register,handleSubmit,reset,formState:{isSubmitting:isLoading,errors}} = useForm<FieldValues>({mode:'onChange',
+    defaultValues:{
+      logo:'',
+      workspaceName:'',
+  }})
+
+
+
   return (
     <Card
       className='w-[800px] h-screen sm:h-auto'
@@ -36,7 +51,48 @@ const DashboardSetup:React.FC<DashboardSetupProps> = ({
           <div
             className='flex flex-col gap-4'
           >
-
+            <div
+              className='flex items-center gap-4'
+            > 
+              <div className='text-5xl'>
+                <EmojiPicker getValue={(emoji) => setSelectedEmoji(emoji)}>
+                  {selectedEmoji}
+                </EmojiPicker>
+              </div>
+              <div className='w-full'>
+                <Label htmlFor='workspaceName' className='text-sm text-muted-foreground'>
+                  Name
+                </Label>
+                <Input 
+                  id='workspaceName' 
+                  type='text' 
+                  placeholder='Workspace Name' 
+                  disabled={isLoading}
+                  className='bg-transparent'
+                  {...register('workspaceName', {required:'Workspace Name is Required.'})}  
+                />
+                <small className='text-red-600'>
+                  {errors?.workspaceName?.message?.toString()}
+                </small>
+              </div>
+            </div>
+            <div>
+              <Label htmlFor='logo' className='text-sm text-muted-foreground'>
+                  Workspace Logo
+                </Label>
+                <Input 
+                  id='logo' 
+                  type='file'
+                  accept='image/*' 
+                  placeholder='Workspace Name' 
+                  disabled={isLoading || subscription?.status !== 'active'}
+                  className='bg-transparent'
+                  {...register('logo', {required:'Workspace Name is Required.'})}  
+                />
+                <small className='text-red-600'>
+                  {errors?.logo?.message?.toString()}
+                </small>
+            </div>
           </div>
         </form>
       </CardContent>
