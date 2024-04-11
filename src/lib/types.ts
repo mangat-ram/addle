e18ed3fad3b3208b8bb5daa132af7fe140b5cfb2
mangat-ram <se.mangat.ram@gmail.com>
2024-04-11 20:46:05 +0530

    Progress Commit.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const FormSchema = z.object(
     {
-      email: z.string().describe('Email').email({message: 'Invalid  Email'}),
+      email: z.string().describe('Email').email({message: 'Invalid Email'}),
       password: z.string().describe('Password').min(1, 'Password is Required'),
     }
   )
