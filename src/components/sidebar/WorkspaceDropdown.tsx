95e91408d33d3a7b281f129e857bfaa695600c7d
mangat-ram <se.mangat.ram@gmail.com>
2024-04-13 16:53:50 +0530

    Working on workspace Dropdown

--- src/components/sidebar/WorkspaceDropdown.tsx
+++ src/components/sidebar/WorkspaceDropdown.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from 'react'
 import { workspace } from '@/lib/supabase/supabase.types';
 
 interface WorkspaceDropdownProps {
@@ -11,8 +13,23 @@ interface WorkspaceDropdownProps {
 const WorkspaceDropdown:React.FC<WorkspaceDropdownProps> = ({
   privateWorkspaces,
   sharedWorkspaces,
-  collaboratingWorkspaces
+  collaboratingWorkspaces,
+  defaultValues
 }) => {
+
+  const { dispatch, state} = useAppState();
+  const [selectedOption, setSelectedOption] = useState(defaultValues)
+  const [isOpen,setIsOpen] = useState(false)
+
+  useEffect(() => {
+    if(!state.workspaces.length){
+      dispatch({type:})
+    }
+  },[
+    privateWorkspaces,
+    sharedWorkspaces,
+    collaboratingWorkspaces
+  ])
   return (
     <div>WorkspaceDropdown</div>
   )
